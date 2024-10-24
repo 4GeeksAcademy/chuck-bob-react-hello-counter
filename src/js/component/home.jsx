fcc5f1cb5feb34d688dd4259dae6ed6b3745916f
Chuck-Bob <derekaassaf@gmail.com>
2024-10-24 20:39:46 +0000

    first

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+	let counter = 0;
+	let tensCounter = 0;
+	let hunCounter = 0;
+	let thouCounter = 0;
+	let tenThouCounter = 0;
+	let hunThouCounter = 0;
+
+	function updateCounter() {
+		document.getElementById('app').innerText = hunThouCounter + "" + tenThouCounter + "" + thouCounter + "" + hunCounter + "" + tensCounter + "" + counter   //display on the page
+
+		console.log(counter); // This will display the counter value in the console
+		counter++;
+
+		if (counter === 10) {
+			counter = 0
+			tensCounter += 1;
+		  }
+		if (tensCounter === 10 ){
+			tensCounter = 0;
+			hunCounter += 1;
+		}
+		if (hunCounter === 10 ){
+			hunCounter = 0;
+			thouCounter += 1;
+		}
+		if (thouCounter === 10 ){
+			thouCounter = 0;
+			tenThouCounter += 1;
+		}
+		if (tenThouCounter === 10 ){
+			tenThouCounter = 0;
+			hunThouCounter += 1;
+		}
+		
+
+		
+	}
+	
+	// Call the updateCounter function every second (1000 milliseconds)
+	setInterval(updateCounter, 10);
+
 };
 
 export default Home;
