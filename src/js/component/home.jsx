2c08e6956ad2b7ec62a32fa0c554dd547b105222
Chuck-Bob <derekaassaf@gmail.com>
2024-10-25 13:40:50 +0000

    three

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,62 +1,90 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 
-const Home = () => {
-	let counter = 0;
-	let tensCounter = 0;
-	let hunCounter = 0;
-	let thouCounter = 0;
-	let tenThouCounter = 0;
-	let hunThouCounter = 0;
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    // Initialize counters
+    this.counter = 0;
+    this.tensCounter = 0;
+    this.hunCounter = 0;
+    this.thouCounter = 0;
+    this.tenThouCounter = 0;
+    this.hunThouCounter = 0;
+    this.interval = null;
+  }
 
-	
-	function updateCounter() {
-		document.getElementById('app').innerHTML =
-		`<div style="display: flex; gap: 10px; background-color: black; color: white; padding: 10px; justify-content: center; font-size: 3rem;">
-		  <div> IMAGE </div>
-		  <div>${hunThouCounter}</div>
-		  <div>${tenThouCounter}</div>
-		  <div>${thouCounter}</div>
-		  <div>${hunCounter}</div>
-		  <div>${tensCounter}</div>
-		  <div>${counter}</div>
-		</div>`; // Display on the page
+  componentDidMount() {
+    // Start the interval when the component mounts
+    this.interval = setInterval(() => {
+      this.updateCounter();
+    }, 10); // Set to 1000 ms (.01 second)
+  }
 
-	// function updateCounter() {
-	// 	// document.getElementById('app').innerHTML = hunThouCounter + "" + tenThouCounter + "" + thouCounter + "" + hunCounter + "" + tensCounter + "" + counter   //display on the page
-	// 	document.getElementById('app').innerHTML = displayCount //display on the page
+  componentWillUnmount() {
+    // Clear interval to prevent memory leaks
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
 
+  updateCounter() {
+    // Increment the counters
+    this.counter++;
 
-		console.log(counter); // This will display the counter value in the console
-		counter++;
+    if (this.counter === 10) {
+      this.counter = 0;
+      this.tensCounter += 1;
+    }
+    if (this.tensCounter === 10) {
+      this.tensCounter = 0;
+      this.hunCounter += 1;
+    }
+    if (this.hunCounter === 10) {
+      this.hunCounter = 0;
+      this.thouCounter += 1;
+    }
+    if (this.thouCounter === 10) {
+      this.thouCounter = 0;
+      this.tenThouCounter += 1;
+    }
+    if (this.tenThouCounter === 10) {
+      this.tenThouCounter = 0;
+      this.hunThouCounter += 1;
+    }
 
-		if (counter === 10) {
-			counter = 0
-			tensCounter += 1;
-		  }
-		if (tensCounter === 10 ){
-			tensCounter = 0;
-			hunCounter += 1;
-		}
-		if (hunCounter === 10 ){
-			hunCounter = 0;
-			thouCounter += 1;
-		}
-		if (thouCounter === 10 ){
-			thouCounter = 0;
-			tenThouCounter += 1;
-		}
-		if (tenThouCounter === 10 ){
-			tenThouCounter = 0;
-			hunThouCounter += 1;
-		}
-		
+    // Force a re-render by calling setState
+    this.forceUpdate();
+  }
 
-		
-	}
-	
-	// Call the updateCounter function every second (1000 milliseconds)
-	setInterval(updateCounter, 10);
-
-};
+  render() {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          gap: '10px',
+          backgroundColor: 'black',
+          color: 'white',
+          padding: '3px',
+          justifyContent: 'center',
+          fontSize: '4rem',
+        }}
+      >
+        <div style={{ backgroundColor: 'grey', padding: '10px' }}>
+  			<img
+    			src="https://www.citypng.com/public/uploads/preview/white-clock-icon-symbol-png-11639648062guyflnf40u.png"
+    			alt="Example Image"
+    			style={{ width: '100px', height: '100px' }} 
+  			/>
+		</div>
+        <div style={{ backgroundColor: 'grey', padding: '10px' }}>{this.hunThouCounter}</div>
+        <div style={{ backgroundColor: 'grey', padding: '10px' }}>{this.tenThouCounter}</div>
+        <div style={{ backgroundColor: 'grey', padding: '10px' }}>{this.thouCounter}</div>
+        <div style={{ backgroundColor: 'grey', padding: '10px' }}>{this.hunCounter}</div>
+        <div style={{ backgroundColor: 'grey', padding: '10px' }}>{this.tensCounter}</div>
+        <div style={{ backgroundColor: 'grey', padding: '10px' }}>{this.counter}</div>
+      </div>
+    );
+  }
+}
 
 export default Home;
